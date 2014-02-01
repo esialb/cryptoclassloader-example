e7954d18f34e1039f529ce2077ce1cb2d2bff9ef
robin <robin.kirkman@gmail.com>
2014-02-01 06:00:36 -0500

    Encrypted jar can load resources (as streams) from itself

--- example-cryptoclassloader-impl/src/main/java/org/cryptoclassloader/example/impl/WidgetImpl.java
+++ example-cryptoclassloader-impl/src/main/java/org/cryptoclassloader/example/impl/WidgetImpl.java
@@ -1,11 +1,24 @@
 package org.cryptoclassloader.example.impl;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.cryptoclassloader.example.api.Widget;
 
 public class WidgetImpl implements Widget {
 
 	public void doSomething() {
-		System.out.println("Hi there!");
+		InputStream in = WidgetImpl.class.getResourceAsStream("widget.txt");
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try {
+			byte[] buf = new byte[1024];
+			for(int r = in.read(buf); r != -1; r = in.read(buf))
+				out.write(buf, 0, r);
+		} catch(IOException ioe) {
+			throw new RuntimeException(ioe);
+		}
+		System.out.println(new String(out.toByteArray()));
 	}
 
 }
